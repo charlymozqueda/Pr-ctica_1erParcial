1ed407eadc7b391d92267b035cfbc7e5cadc4102
AlexisR <rodex1204@gmail.com>
2023-10-26 21:39:37 -0600

    Menu y frase

---
+++ funciones.js
@@ -0,0 +1,13 @@
+const temaOscuro = () => {
+    document.querySelector("body").setAttribute
+    ("data-bs-theme", "dark");
+}
+
+const temaLuz = () => {
+    document.querySelector("body").setAttribute
+    ("data-bs-theme", "light");
+}
+
+const cambiarTema = () => {
+    document.querySelector("body").getAttribute("data-bs-theme") === "light"? temaOscuro() : temaLuz();
+}
